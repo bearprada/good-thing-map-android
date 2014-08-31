1aec82f38e4d57aeb114b57104bcc9477403db9f
PRADA Hsiung <bear.prada@gmail.com>
2014-08-31 11:32:55 +0800

    migrate dns to goodthing.tw

--- app/src/main/java/goodthingmap/android/prada/lab/goodthingmap/Consts.java
+++ app/src/main/java/goodthingmap/android/prada/lab/goodthingmap/Consts.java
@@ -4,5 +4,5 @@ package goodthingmap.android.prada.lab.goodthingmap;
  * Created by prada on 2014/8/31.
  */
 public class Consts {
-    public static final String AUTHORITY = "http://60.199.245.21:8080/";
+    public static final String AUTHORITY = "http://goodthing.tw:8080/";
 }
