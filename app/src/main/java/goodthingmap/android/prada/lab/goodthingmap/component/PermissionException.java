cc06fa1e484bd3d5dcef7da7fe2315a66372c9fc
nonumpa <acerxp511@msn.com>
2016-03-27 20:29:34 +0800

    Update AlertDialog and GPSLocation
    1.Replace original dialog with Material-dialog lib
    and update SdkVersion to  API 23
    2.Update GPSLocation implementation
    3.Cancel image scale function

---
+++ app/src/main/java/goodthingmap/android/prada/lab/goodthingmap/component/PermissionException.java
@@ -0,0 +1,7 @@
+package goodthingmap.android.prada.lab.goodthingmap.component;
+
+public class PermissionException extends Exception {
+  public PermissionException(String message) {
+    super(message);
+  }
+}
